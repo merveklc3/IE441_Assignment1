6f192e16bc702c723978c9982bf6f235de174d00
Merve Sude Kılıç <mervesudeklc@gmail.com>
2025-10-17 20:35:45 +0300

    README

---
+++ readme.py
@@ -0,0 +1,7 @@
+echo "# IE441_Assignment1" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/merveklc3/IE441_Assignment1.git
+git push -u origin main
